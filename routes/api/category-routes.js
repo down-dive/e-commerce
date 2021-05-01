dc553b10022aa49661cde03f14418ad60b8383bd
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-05-01 15:08:31 -0700

    updated modules and routes for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,15 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({})
+    .then(categories => {
+      console.log(categories)
+      res.json(categories)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
